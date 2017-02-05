ada0b84085885a002b387d76ebcdc532331d477d
gvukasin <gabrielle.vukasin@tufts.edu>
2017-02-05 13:14:49 -0800

    Add header files

---
+++ Headers/SPI.h
@@ -0,0 +1,18 @@
+/****************************************************************************
+ 
+  Header file for SPI 
+  based on the Gen 2 Events and Services Framework
+
+ ****************************************************************************/
+
+#ifndef SPI_H
+#define SPI_H
+
+#include "ES_Configure.h"
+#include "ES_Types.h"
+
+// Public Function Prototypes
+
+
+#endif /* SPI_H */
+
