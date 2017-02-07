b0202563d5c391ad08d4053ac747f05b9286c298
WIN\jiongzg <jiongzg>
2017-02-06 22:17:24 -0800

    New Tape Module started

---
+++ TapeModule.h
@@ -0,0 +1,20 @@
+/****************************************************************************
+ 
+  Header file for Tape Module
+ ****************************************************************************/
+
+#ifndef TapeModule_H
+#define TapeModule_H
+
+// Event Definitions
+#include "ES_Configure.h" /* gets us event definitions */
+#include "ES_Types.h"     /* gets bool type for returns */
+
+// Public Function Prototypes
+void InitTapeInterrupt (void);
+void TapeInterruptResponse(void);
+
+// Module Function Prototypes
+
+#endif 
+
