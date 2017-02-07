5a0c63a014df6e1aad06d6e7035cc9543c2d50ca
egalbally-PC\egalbally <elenagal@stanford.edu>
2017-02-06 20:09:12 -0800

    Changed SPI name to SPIService

---
+++ Headers/SPIService.h
@@ -0,0 +1,20 @@
+/****************************************************************************
+  Header file for SPIService
+  based on the Gen 2 Events and Services Framework
+ ****************************************************************************/
+
+#ifndef SPIService_H
+#define SPIService_H
+
+#include "ES_Configure.h"
+#include "ES_Types.h"
+#include "ES_Events.h" 
+
+// Public Function Prototypes
+bool InitSPIService ( uint8_t );
+ES_Event RunSPIService( ES_Event );
+bool PostSPIService( ES_Event );
+void WriteSPI( uint8_t );
+
+#endif /* SPIService_H */
+
